7462d531c0dbf14019f9fc6431fa579d44768f92
reno-ir <renecas@gmail.com>
2023-03-23 11:15:33 -0500

    Test

--- app/src/main/java/com/kojingu/xylo/screens/WelcomeScreen.kt
+++ app/src/main/java/com/kojingu/xylo/screens/WelcomeScreen.kt
@@ -1,32 +1,36 @@
-package com.kojingu.xylo.Screens
+package com.kojingu.xylo.screens
 
-import android.support.v4.os.IResultReceiver.Default
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kojingu.xylo.R
 
 @Composable
-fun WelcomeScreen(){
+fun WelcomeScreen() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Title()
-        Buttons()
+        Buttons(
+            onJoinClick = {},
+            onNewGameClick = {}
+        )
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun Title() {
     Surface(
@@ -34,30 +38,48 @@ fun Title() {
         modifier = Modifier.padding(bottom = 160.dp, top = 170.dp)
     ) {
         Text(
-            text = "Xylo",
+            text = stringResource(R.string.title_app),
             fontSize = 80.sp,
-            fontWeight = FontWeight.Bold)
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun Buttons(){
+fun Buttons(
+    onJoinClick: () -> Unit,
+    onNewGameClick: () -> Unit
+) {
     Column(
         modifier = Modifier.padding(top = 145.dp)
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { onJoinClick() },
             contentPadding = PaddingValues(horizontal = 24.5.dp, vertical = 18.dp),
             modifier = Modifier.padding(bottom = 18.dp)
         ) {
-            Text(text = "Join your friends")
+            Text(text = stringResource(R.string.button_join))
         }
         OutlinedButton(
-            onClick = { /*TODO*/ },
-            contentPadding = PaddingValues(18.dp)
+            onClick = { onNewGameClick() }, contentPadding = PaddingValues(18.dp)
         ) {
-            Text(text = "Create a new game")
+            Text(text = stringResource(R.string.button_new_game))
         }
     }
 }
+
+@Preview
+@Composable
+private fun PreviewTitle() {
+    Title()
+}
+
+@Preview
+@Composable
+private fun PreviewButtons() {
+    Buttons(
+        onJoinClick = {},
+        onNewGameClick = {}
+    )
+}
+
