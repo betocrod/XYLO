eab481048ed14777f0cc8581069cd794deac7fd6
reno-ir <renecas1996@gmail.com>
2023-03-24 19:02:25 -0500

    First entrega

--- app/src/main/java/com/kojingu/xylo/screens/WelcomeScreen.kt
+++ app/src/main/java/com/kojingu/xylo/screens/WelcomeScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kojingu.xylo.R
-
 @Composable
 fun WelcomeScreen() {
     Column(
