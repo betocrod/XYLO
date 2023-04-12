2f5e9bd9aac098d91a0e0308bb7901a54a5c1b1d
reno-ir <renecas1996@gmail.com>
2023-04-12 12:42:36 -0500

    xylo-24-create-new-game

---
+++ app/src/main/java/com/kojingu/xylo/creategamescreen/TitleCreateGame.kt
@@ -0,0 +1,27 @@
+package com.kojingu.xylo.creategamescreen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kojingu.xylo.R
+
+@Composable
+fun TitleCreateGame(
+    modifier: Modifier = Modifier
+){
+    Surface(
+        modifier = modifier.padding(top = 40.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.title_create_game),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
+}
