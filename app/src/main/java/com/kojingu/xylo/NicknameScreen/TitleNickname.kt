2435035a784e7b3f832a3a533086a56e18050959
reno-ir <renecas1996@gmail.com>
2023-04-12 15:09:04 -0500

    xylo-26-nickname-screen

---
+++ app/src/main/java/com/kojingu/xylo/NicknameScreen/TitleNickname.kt
@@ -0,0 +1,23 @@
+package com.kojingu.xylo.NicknameScreen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kojingu.xylo.R
+
+@Composable
+fun TitleNickname(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier.padding(top = 25.dp),
+        text = stringResource(R.string.title_nickname_screen),
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    )
+}
